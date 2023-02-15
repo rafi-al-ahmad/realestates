d9099be4f1a097ff30c8ce3ab3530b4502aa6958
rafi <rafi@mail.com>
2023-02-15 18:22:47 +0300

    change favicon

--- resources/views/frontpage/layouts/styles.blade.php
+++ resources/views/frontpage/layouts/styles.blade.php
@@ -5,8 +5,9 @@
 <link rel="stylesheet" href="{{url('frontpage')}}/css/responsive.css">
 
 <!-- Favicon -->
-<link href="{{url('frontpage')}}/images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
-<link href="{{url('frontpage')}}/images/favicon.ico" sizes="128x128" rel="shortcut icon" />
+<link href="{{url('/')}}/assets/images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
+<link href="{{url('/')}}/assets/images/favicon.ico" sizes="128x128" rel="shortcut icon" />
+
 <style>
     .home5_bgi5 {
         background-image: url("{{url('assets/frontpage/images/home/home-1920x960.jpg')}}");
