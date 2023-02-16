e906d7b2b2138d871f63d091974ec6e3ea8bef85
rafi <rafi@mail.com>
2023-02-16 17:33:32 +0300

    fix description not shoing issue

--- resources/views/frontpage/pages/property-details.blade.php
+++ resources/views/frontpage/pages/property-details.blade.php
@@ -97,7 +97,7 @@
                                 </ul>
                             </div>
                             <h4 class="mb30">{{__('Description')}}</h4>
-                            <p class="mb25">{$property->description}}</p>
+                            <p class="mb25">{{$property->description}}</p>
                             <!-- <div class="collapse" id="collapseExample">
                                 <div class="card card-body">
                                     <p class="mt10 mb10">Fully furnished. Elegantly appointed condominium unit situated on premier location. PS6. The wide entry hall leads to a large living room with dining area. This expansive 2 bedroom and 2 renovated marble bathroom apartment has great windows. Despite the interior views, the apartments Southern and Eastern exposures allow for lovely natural light to fill every room. The master suite is surrounded by handcrafted milkwork and features incredible walk-in closet and storage space.</p>
