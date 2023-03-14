2359287125557c693cd13a46c193382be959447f
rafi <rafi@mail.com>
2023-03-14 17:16:59 +0300

    add chat bot

--- resources/views/frontpage/layouts/index.blade.php
+++ resources/views/frontpage/layouts/index.blade.php
@@ -23,6 +23,25 @@
         gtag('js', new Date());
         gtag('config', 'G-T87LFL4GQN');
     </script>
+
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'da9b549793f518ad4c8cef0ce0dbb9809d51af7a';
+        window.smartsupp || (function(d) {
+            var s, c, o = smartsupp = function() {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+    </script>
 </head>
 
 <body>
