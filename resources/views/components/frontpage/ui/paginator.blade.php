186717e6a5057b3024f82feb73748b1917b540d5
rafi <rafi@mail.com>
2023-03-31 15:24:11 +0300

    fix paginator

--- resources/views/components/frontpage/ui/paginator.blade.php
+++ resources/views/components/frontpage/ui/paginator.blade.php
@@ -1,7 +1,7 @@
 <div class="mbp_pagination mt20">
     <ul class="page_navigation">
         <li class="page-item {{$pagination->previousPageUrl()? '':'disabled'}}">
-            <a class="page-link" href="{{$pagination->previousPageUrl()}}" tabindex="-1" aria-disabled="true"> <span class="flaticon-left-arrow"></span> Prev</a>
+            <a class="page-link" href="{{$pagination->previousPageUrl()}}"> <span class="flaticon-left-arrow"></span></a>
         </li>
         @for($i = 0, $page = 1; $i < $pagination->total(); $page++ , $i += $pagination->perPage())
             <li class="page-item {{$pagination->currentPage() == $page? 'active ':''}}"><a class="page-link" href="{{$pagination->url($page)}}">{{$page}}</a></li>
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group([
     'middleware' => ['auth']
 ], function () {
