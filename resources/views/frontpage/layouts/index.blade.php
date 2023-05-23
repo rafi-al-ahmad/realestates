d1c4d1818eeb31f87d13c8f45364c9a7f1ff795b
rafi <rafi@mail.com>
2023-05-23 19:18:36 +0300

    update GTag

--- resources/views/frontpage/layouts/index.blade.php
+++ resources/views/frontpage/layouts/index.blade.php
@@ -34,30 +34,12 @@
             });
             var f = d.getElementsByTagName(s)[0],
                 j = d.createElement(s),
-                dl = l != 'dataLayer' ? ' & l = ' + l : '';
-            j.async = true;
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-THF39NF');
-    </script>
-    <!-- End Google Tag Manager -->
-
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
                 dl = l != 'dataLayer' ? '&l=' + l : '';
             j.async = true;
             j.src =
                 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
             f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-THF39NF');
+        })(window, document, 'script', 'dataLayer', 'GTM-5H7MQ55');
     </script>
     <!-- End Google Tag Manager -->
 
@@ -83,10 +65,6 @@
 </head>
 
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THF39NF" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
     <div class="wrapper">
         <div class="preloader"></div>
         @include("frontpage.layouts.navbar")
